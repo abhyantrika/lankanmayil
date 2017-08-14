940d419338719b9bb6ce9fb93cf8e2ef9bf74aea
Codeass <hjkpidiot@gmail.com>
2017-08-15 01:52:11 +0530

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,12 +1,12 @@
 MONGO_ADDRESS = 'localhost'
-MONGO_USERNAME = 'root'
-MONGO_PASSWORD ='password'
+MONGO_USERNAME = ''
+MONGO_PASSWORD =''
 
-WAY2SMS_USERNAME = '8867500207'
-WAY2SMS_PASSWORD = 'creathives123'
+WAY2SMS_USERNAME = ''
+WAY2SMS_PASSWORD = ''
 
-SENDER_MAIL = 'info.lankanmayil@gmail.com'
-SENDER_PASSWORD = 'creathives123'
+SENDER_MAIL = ''
+SENDER_PASSWORD = ''
 
-ADMIN_MAIL = 'manojkumarkalaivanan@gmail.com'
-ADMIN_NUMBER = '8867500207'
+ADMIN_MAIL = ''
+ADMIN_NUMBER = ''
